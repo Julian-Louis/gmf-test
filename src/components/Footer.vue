12acdf6b73139851a62ddb90251806fac5e20ed4
Julian-Louis <contact@julianlouis.fr>
2021-08-09 02:11:40 +0200

    Added margin top to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,3 @@
 <template>
-    <div class="flex justify-center absolute bottom-0 min-w-full text-grey"><p>Certification de <span class="text-discord font-semibold bg-discord bg-opacity-10">@Sumonil#2016</span> - Garry's Mod France</p></div>
+    <div class="flex justify-center mt-16 min-w-full text-grey"><p>Certification de <span class="text-discord font-semibold bg-discord bg-opacity-10">@Sumonil#2016</span> - Garry's Mod France</p></div>
 </template>
