30dd94db2baad9a37f3b4541612662ae7cdadfd7
Julian-Louis <contact@julianlouis.fr>
2021-08-09 02:10:17 +0200

    Started Youtube modal component

---
+++ src/components/YoutubeChannelModal.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex items-center justify-center">
+    <div
+      class="
+        absolute
+        rounded-lg
+        bg-white
+        top-32
+        shadow-2xl
+        w-1/3
+        flex
+        items-center
+        justify-center
+        flex-col
+      "
+    >
+      <img
+        class="rounded-full h-32 mt-8"
+        src="https://yt3.ggpht.com/ytc/AKedOLTvz_iPwMT8SUj8NIb4F3nBssN0E7tykps9TXcdYw=s176-c-k-c0x00ffffff-no-rj"
+        alt=""
+      />
+      <h1 class="font-medium text-2xl">SQUEEZIE</h1>
+      <p class="mt-4">Ceci est la description de la chaîne youtube</p>
+      <span class="mt-2 flex items-center text-black text-opacity-60"
+        ><ClockIcon class="h-4" />Crée le dimanche 9 janvier 2011</span
+      >
+
+      <div class="flex flex-wrap gap-1 text-white text-center mt-8">
+        <div style="flex-basis: 48%" class="px-16 bg-primary rounded-md">salut</div>
+        <div style="flex-basis: 48%" class="p-8 bg-primary rounded-md">salut</div>
+        <div style="flex-basis: 48%" class="p-8 bg-primary rounded-md">salut</div>
+      </div>
+    </div>
+  </div>
+</template>
