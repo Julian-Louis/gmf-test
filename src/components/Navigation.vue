9623577923d34dea8ddb347c234470aa8fd2cf09
Julian-Louis <contact@julianlouis.fr>
2021-08-09 02:11:29 +0200

    Created navigation component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="flex py-6">
+    <div class="flex-1">
+      <img class="h-12 ml-54" src="/gmod.png" alt="Garry's Mod Logo" />
+    </div>
+    <div class="flex-1">
+      <ul class="font-medium inline text-lg">
+        <li><a href="/" class="text-white inline p-6">Accueil</a></li>
+        <li>
+          <a
+            href="https://discord.com"
+            class="text-white text-opacity-60 inline p-6"
+            >Discord</a
+          >
+        </li>
+        <li>
+          <div class="flex items-center border border-white border-2 rounded-full">
+            <input v-model="input"  @keyup.enter="submit" 
+              type="text"
+              placeholder="Chaîne youtube"
+              class="
+                rounded-full
+                bg-transparent
+                text-white text-base
+                py-6px
+                px-4
+                placeholder-white placeholder-opacity-60
+              "
+            />
+            <SearchIcon class="mr-2 h-4" />
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<style>
+li {
+  float: left;
+}
+</style>
+
+<script lang="ts">
+  import { defineComponent } from "vue";
+  
+  export default defineComponent({
+    data(){
+      return{
+        input: "",
+      }
+    },
+    emits: ['input'],
+
+    methods:{
+      submit(){
+        console.log(this.input)
+        this.$emit('input',this.input)
+      }
+    }
+  })
+</script>
