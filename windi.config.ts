db581f2a15a093cc7651fe3297800df5a3f677ae
Julian-Louis <contact@julianlouis.fr>
2021-08-09 02:10:54 +0200

    Added few color

--- windi.config.ts
+++ windi.config.ts
@@ -13,8 +13,11 @@ export default defineConfig({
             gr1: "#0055f5",
             gr2: "#40a4fd",
             white: "#ffffff",
+            black: "#000000",
             grey:"#858586",
-            discord:"#5865F2"
+            discord:"#5865F2",
+            transparent: "transparent",
+            primary: "#1768F6"
 
         }
     },
